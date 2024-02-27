3400b4afd9acd7113270b8487fe6213a5954082d
Aleksandr Yamshchikov <sashayamschik0v@gmail.com>
2024-02-28 01:12:53 +0300

    configured the interaction of the theme switcher with redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import langReducer from '../features/langSlice'
+import themeReducer from '../features/themeSlice'
 
 export default configureStore({
   reducer: {
     language: langReducer,
+    theme: themeReducer,
   },
 })
---
+++ src/features/themeSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+const getTheme = () => {
+    const theme = `${window?.localStorage?.getItem('theme')}`
+    if (['light', 'dark'].includes(theme)) return theme
+
+    const userMedia = window.matchMedia('(prefers-color-scheme: dark)')
+    if (userMedia.matches) return 'dark'
+
+    return 'light'
+}
+
+const initialState = getTheme();
+
+const themeSlice = createSlice({
+    name: 'theme',
+    initialState,
+    reducers: {
+        set: (state, action) => action.payload,
+    }
+})
+export const { set } = themeSlice.actions
+export default themeSlice.reducer
